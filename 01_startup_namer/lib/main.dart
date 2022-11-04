b8e99ba1fc1f5d2b077ef742fc87c09e5bb71cdf
Volodymyr (Vlad) Chapman <44582194+VolodymyrChapman@users.noreply.github.com>
2022-11-04 23:44:31 +0000

    01_startup_namer complete

--- 01_startup_namer/lib/main.dart
+++ 01_startup_namer/lib/main.dart
@@ -8,19 +8,17 @@ void main() {
 class MyApp extends StatelessWidget {
   // stateless widgets are immutable
   const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Welcome to Flutter',
+      title: 'Startup Name Generator',
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Welcome to Flutter'),
+          title: const Text('Startup Name Generator'),
         ),
         body: const Center(
-            // no const preceding Center as now val dependent
-            // on variable
-            child: RandomWords()),
+          child: RandomWords(),
+        ),
       ),
     );
   }
@@ -37,11 +35,28 @@ class RandomWords extends StatefulWidget {
 }
 
 class _RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+  final _biggerFont = const TextStyle(fontSize: 18); // TextStyle widget
+  // will be essential in future
+
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random(); //initiate word pairs
-    return Text(wordPair.asPascalCase); // 'method' on wordpair object
-    // Pascal case / upper camel case makes first letter of each word
-    // capital
+    return ListView.builder(
+      padding: const EdgeInsets.all(16.0),
+      itemBuilder: (context, i) {
+        if (i.isOdd) return const Divider();
+
+        final index = i ~/ 2;
+        if (index >= _suggestions.length) {
+          _suggestions.addAll(generateWordPairs().take(10));
+        }
+        return ListTile(
+          title: Text(
+            _suggestions[index].asPascalCase,
+            style: _biggerFont,
+          ),
+        );
+      },
+    );
   }
 }
